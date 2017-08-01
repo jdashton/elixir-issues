ab3e58011b4f6f76d305b6b295d4965bd0e92b4f
J. Daniel Ashton <jdashton@jda11.nadadventist.org>
2017-07-31 23:00:38 -0400

    end of chapter

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,5 +1,7 @@
 defmodule Issues.CLI do
 
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
@@ -8,7 +10,7 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -26,17 +28,10 @@ defmodule Issues.CLI do
     parse = OptionParser.parse(argv, switches: [ help: :boolean],
                                      aliases:  [ h:    :help   ])
     case parse do
-
-      { [ help: true ], _, _ }
-        -> :help
-
-      { _, [ user, project, count ], _ }
-        -> { user, project, String.to_integer(count) }
-
-      { _, [ user, project ], _ }
-        -> { user, project, @default_count }
-
-      _ -> :help
+      { [ help: true ], _,           _ } -> :help
+      { _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
+      { _, [ user, project ],        _ } -> { user, project, @default_count }
+      _                                  -> :help
     end
   end
 
@@ -47,8 +42,24 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
-    Issues.GitHubIssues.fetch(user, project)
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> Map.get(i1, "created_at") <= Map.get(i2, "created_at") end
   end
 end
 
